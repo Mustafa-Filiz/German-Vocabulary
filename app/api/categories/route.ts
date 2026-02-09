78200dcf7b5d6e968a1a272eaa262629d0fb996a
Mustafa  Filiz <mustafafiliz18@gmail.com>
2026-02-10 00:11:43 +0100

    feat: refactor category API response structure and update fetching logic

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -2,6 +2,7 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  const response = [];
   const categories = await prisma.word.groupBy({
     by: ["category"],
     _count: { id: true },
@@ -13,12 +14,14 @@ export async function GET() {
     },
   });
 
-  return NextResponse.json({
-    categories: categories
-      .map((cat) => ({
-        categoryTitle: cat.category!,
+  for (const cat of categories) {
+    if (cat.category) {
+      response.push({
+        categoryTitle: cat.category,
         wordCount: cat._count.id,
-      }))
-      .filter(Boolean),
-  });
+      });
+    }
+  }
+
+  return NextResponse.json(response);
 }
--- features/learn/data/get-categories.ts
+++ features/learn/data/get-categories.ts
@@ -2,19 +2,17 @@ import { typedFetch } from "@/lib/typed-fetch";
 import { categoryEnum } from "@/types";
 import z from "zod";
 
-const categoriesResponseSchema = z.object({
-  categories: z.array(
-    z.object({
-      categoryTitle: categoryEnum,
-      wordCount: z.number(),
-    }),
-  ),
-});
+const categoriesResponseSchema = z.array(
+  z.object({
+    categoryTitle: categoryEnum,
+    wordCount: z.number(),
+  }),
+);
 
 export async function getCategories() {
   const response = await typedFetch(
     "/api/categories",
     categoriesResponseSchema,
   );
-  return response.categories;
+  return response;
 }
